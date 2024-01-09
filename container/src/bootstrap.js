57ce33df03c1268af4612b95df99925ae7dcabd5
discoveranirban <discoveranirban@gmail.com>
2024-01-09 21:21:11 +0530

    remote module consumption

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,6 +1,7 @@
 import { mount } from "products/ProductsIndex";
-import "cart/CartShow";
+import { mount as cartMount } from "cart/CartShow";
 
 console.log("container");
 
 mount(document.querySelector('#dev-products'));
+cartMount(document.querySelector('#dev-cart'));
